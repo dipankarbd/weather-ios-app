f4aeac937a1eb6e07614a47bb441a412d06ce71d
Dipankar Biswas <dipankar_cse@yahoo.com>
2024-03-10 15:11:02 +0600

    initial commit

---
+++ WeatherApp.iOS/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_iOSApp.swift
+//  WeatherApp.iOS
+//
+//  Created by Dipankar Biswas on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
